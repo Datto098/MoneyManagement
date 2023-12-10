43e3c55d4c76804ca693df4f7fea38b6920c9951
ttchien <chientt.00.student@fit.tdc.edu.vn>
2023-12-10 23:07:29 +0700

    xong layout

---
+++ main/java/com/example/moneymanagement/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.moneymanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ghichep_layout);
+
+    }
+}
